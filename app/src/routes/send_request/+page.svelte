38f3e93445622aa3f0efb677a2f4b8226e16c9db
thekaroe-thailand <thekaroe@gmail.com>
2024-07-19 07:05:38 +0700

    day4

---
+++ app/src/routes/send_request/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+  import axios from "axios";
+  import config from "../../config";
+
+  async function sendGet() {
+    await axios.get(config.apiPath + "/api/book/list");
+  }
+
+  const sendPost = async () => {
+    const payload = {
+      id: 100,
+      name: "kob",
+      salary: 200000,
+    };
+    await axios.post(config.apiPath + "/api/book/create", payload);
+  };
+
+  async function sendPut() {
+    const payload = {
+      id: 100,
+      name: "kob",
+      salary: 1000,
+    };
+
+    await axios.put(config.apiPath + "/api/book/update/100", payload);
+  }
+
+  async function sendDelete() {
+    await axios.delete(config.apiPath + "/api/book/delete/100");
+  }
+</script>
+
+<div class="pt-5 text-center">
+  <button class="btn btn-primary" on:click={() => sendGet()}>GET Method</button>
+  <button class="btn btn-success" on:click={() => sendPost()}>
+    POST Method
+  </button>
+  <button class="btn btn-info" on:click={() => sendPut()}>PUT Method</button>
+  <button class="btn btn-danger" on:click={() => sendDelete()}>
+    DELETE Method
+  </button>
+</div>
---
+++ app/src/routes/upload_file/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+  import axios from "axios";
+  import config from "../../config";
+
+  let yourFile = null;
+
+  async function sendFileToSerer() {
+    let formData = new FormData();
+
+    if (yourFile !== undefined) {
+      if (yourFile.length > 0) {
+        formData.append("myImage", yourFile[0]);
+        await axios.post(config.apiPath + "/api/user/upload", formData);
+      }
+    }
+  }
+</script>
+
+<div class="container mt-3">
+  <div>Choose Your File</div>
+  <input class="form-control" type="file" bind:files={yourFile} />
+
+  <button class="btn btn-primary" on:click={() => sendFileToSerer()}>
+    Send to Server
+  </button>
+</div>
